9e3200c782523f4b51002c6f1ad71ca030f09320
Donnie Hartman <hartman_donnie@hotmail.com>
2025-06-10 10:25:24 -0400

    :tada: Initial commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-3xl text-blue-500 text-center mt-10">
+      Hello Nuxt + Tailwind
+    </h1>
+    <NuxtRouteAnnouncer />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B3C65",
+        secondary: "#FF6B6B",
+        accent: "#2CE6C3",
+        light: "#FFFCFF",
+        heading: "#2D2F3F",
+        body: "#697EA3",
+      },
+      fontFamily: {
+        inter: ["Inter", "san-serif"],
+        poppins: ["Poppins", "san-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
